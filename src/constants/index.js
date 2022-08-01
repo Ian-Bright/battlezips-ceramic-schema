e495a1dc4f9cfe49a835d38af29e7f960d9311cd
Ian-Bright <ianbrighton1@gmail.com>
2022-08-01 17:41:36 -0400

    ship position format updated

---
+++ src/constants/index.js
@@ -0,0 +1,9 @@
+export const ERROR_MESSAGES = {
+  ACTIVE_GAME_PROPERTY_VALIDATION_MESSAGE: `Validation Error: data/activeGame must have required property 'shipPositions', data/activeGame must have required property 'shots'`,
+  FEWER_ITEMS_MESSAGE: 'must NOT have fewer',
+  INVALID_TYPE_MESSAGE: 'must be number',
+  MISSING_SHIP_PROPERTY_MESSAGE:
+    'data/activeGame/shipPositions must have required propert',
+  MORE_ITEMS_MESSAGE: 'must NOT have more',
+  TOP_LEVEL_VALIDATION_ERROR_MESSAGE: `Validation Error: data must have required property 'activeGame'`,
+};
--- src/tests/test.js
+++ src/tests/test.js
@@ -1,89 +1,169 @@
+import chaiAsPromised from 'chai-as-promised';
 import { CeramicClient } from '@ceramicnetwork/http-client';
 import { DataModel } from '@glazed/datamodel';
 import { DID } from 'dids';
 import { DIDDataStore } from '@glazed/did-datastore';
+import dotenv from 'dotenv';
 import { Ed25519Provider } from 'key-did-provider-ed25519';
-import { fromString } from 'uint8arrays/from-string';
+import { ERROR_MESSAGES } from '../constants/index.js';
+import chai, { expect } from 'chai';
+import { toString } from 'uint8arrays';
 import { getResolver } from 'key-did-resolver';
 import modelAliases from '../deployment/deployment.json' assert { type: 'json' };
-import { expect } from 'chai';
+import { randomBytes } from 'crypto';
+import { arraysEqual } from '../utils/index.js';
 
+chai.use(chaiAsPromised);
+dotenv.config();
 const { API_URL } = process.env;
-
-// TODO: Auto generate DIDs
-// const did1 = 'did:key:z6MkhFtXTbV4wyKZKBCNpEufEbZDpdVryqXhs9LCpGDGq5pT';
-// const did2 = 'did:key:z6MkruWVfxoz8dTo5n3PtazjQqCHZdnpLUxbRsdKimQGxi3c';
-
-const SEED_1 =
-  '33662f17a0c92b674132326b4753e08f1b3be73c0a9c5ea8930e4072469cbb85';
-const SEED_2 =
-  'c04936bc7648cfe7825a5848c05659619addf59405f855527522ae2d0be67cef';
+const TIMEOUT = 20000;
 
 const authenticateDID = async (seed) => {
-  const key = fromString(seed, 'base16');
   const did = new DID({
-    provider: new Ed25519Provider(key),
+    provider: new Ed25519Provider(seed),
     resolver: getResolver(),
   });
   await did.authenticate();
   return did;
 };
 
+const generateSeed = () => {
+  return new Uint8Array(randomBytes(32));
+};
+
 describe('Test Battlezip game', async () => {
-  let ceramic1;
-  let ceramic2;
-  let did1;
-  let did2;
-  let store1;
-  let store2;
+  let ceramic;
+  let did;
+  let store;
 
-  // Authenticate both DIDs
   before(async () => {
-    console.log('Authenticating DID 1...');
-    did1 = await authenticateDID(SEED_1);
-    console.log('DID 1 authenticated.');
-    console.log('Authenticating DID 2...');
-    did2 = await authenticateDID(SEED_2);
-    console.log('DID 2 authenticated.');
-    console.log('API URL: ', API_URL);
-    ceramic1 = new CeramicClient(API_URL);
-    ceramic2 = new CeramicClient(API_URL);
-    ceramic1.did = did1;
-    ceramic2.did = did2;
-    const model = new DataModel({ ceramic: ceramic1, aliases: modelAliases });
-    store1 = new DIDDataStore({ ceramic: ceramic1, model });
-    store2 = new DIDDataStore({ ceramic: ceramic2, model });
+    const seed = generateSeed();
+    console.log('Generated seed: ', toString(seed, 'base16'));
+    did = await authenticateDID(seed);
+    ceramic = new CeramicClient(API_URL);
+    ceramic.did = did;
+    const model = new DataModel({ ceramic: ceramic, aliases: modelAliases });
+    store = new DIDDataStore({ ceramic: ceramic, model });
   });
 
-  describe('Profile states', async () => {
-    it('No games should exist for account1', async () => {
-      const did1Profile = await store1.get('myBattlezipProfile');
+  describe('Initial profile state', async () => {
+    it('No games should exist for upon profile initialization', async () => {
+      const did1Profile = await store.get('myBattlezipProfile');
       expect(did1Profile).to.equal(null);
-    });
-    it('No games should exist for account 2', async () => {
-      const did2Profile = await store2.get('myBattlezipProfile');
-      expect(did2Profile).to.equal(null);
-    });
+    }).timeout(TIMEOUT);
   });
 
-  describe('Test profile assignments', async () => {
-    xit('Should revert if Battlezip profile is initialized without either required parameters', async () => {
+  describe('Test schema property inclusion', async () => {
+    it('Should throw if Battlezip profile is initialized without activeGame', async () => {
       try {
-        await store1.set('myBattlezipProfile', {
+        await store.set('myBattlezipProfile', {
           text: 'Invalid input.',
         });
       } catch (err) {
-        console.log('Error: ', err);
+        // Hacky error check while I determine why Chai's .throw() is not working as expected
+        expect(err.message).to.include(
+          ERROR_MESSAGES.TOP_LEVEL_VALIDATION_ERROR_MESSAGE
+        );
       }
-    }).timeout(10000);
-    it('Should revert if Battlezip profile is initialized without one required parameter', async () => {
+    }).timeout(TIMEOUT);
+    it('Should throw if required properties are not included', async () => {
       try {
-        await store1.set('myBattlezipProfile', {
-          totalGames: 0,
+        await store.set('myBattlezipProfile', {
+          activeGame: {},
         });
       } catch (err) {
-        console.log('Error: ', err);
+        expect(err.message).to.include(
+          ERROR_MESSAGES.ACTIVE_GAME_PROPERTY_VALIDATION_MESSAGE
+        );
       }
-    }).timeout(10000);
+    }).timeout(TIMEOUT);
+  });
+
+  describe('Test input validation on required properties', async () => {
+    it('Should throw if not all required ships are included', async () => {
+      try {
+        await store.set('myBattlezipProfile', {
+          activeGame: {
+            shipPositions: [
+              [0, 0, 0],
+              [0, 0, 0],
+              [0, 0, 0],
+            ],
+            shots: [],
+          },
+        });
+      } catch (err) {
+        expect(err.message).to.include(ERROR_MESSAGES.FEWER_ITEMS_MESSAGE);
+      }
+    }).timeout(TIMEOUT);
+    it('Should throw if ship position array elements are invalid type', async () => {
+      try {
+        await store.set('myBattlezipProfile', {
+          activeGame: {
+            shipPositions: [
+              ['0', '0', '0'],
+              ['0', '0', '0'],
+              ['0', '0', '0'],
+              ['0', '0', '0'],
+              ['0', '0', '0'],
+            ],
+            shots: [],
+          },
+        });
+      } catch (err) {
+        expect(err.message).to.include(ERROR_MESSAGES.INVALID_TYPE_MESSAGE);
+      }
+    }).timeout(TIMEOUT);
+    it('Should throw if shot array elements are invalid type', async () => {
+      try {
+        await store.set('myBattlezipProfile', {
+          activeGame: {
+            shipPositions: [
+              [0, 0, 0],
+              [0, 0, 0],
+              [0, 0, 0],
+              [0, 0, 0],
+              [0, 0, 0],
+            ],
+            shots: [['1', '1', '1']],
+          },
+        });
+      } catch (err) {
+        expect(err.message).to.include(ERROR_MESSAGES.INVALID_TYPE_MESSAGE);
+      }
+    }).timeout(TIMEOUT);
+  });
+
+  describe('Test correct game state storage', async () => {
+    it('Should be able to store correct input and read off of ceramic', async () => {
+      const shot1 = [0, 0, 1];
+      const shot2 = [0, 1, 0];
+
+      await store.set('myBattlezipProfile', {
+        activeGame: {
+          shipPositions: [
+            [0, 0, 0],
+            [0, 0, 0],
+            [0, 0, 0],
+            [0, 0, 0],
+            [0, 0, 0],
+          ],
+          shots: [shot1, shot2],
+        },
+      });
+      const res = await store.get('myBattlezipProfile');
+      expect(arraysEqual([0, 0, 0], res.activeGame.shipPositions[0])).to.be
+        .true;
+      expect(arraysEqual([0, 0, 0], res.activeGame.shipPositions[1])).to.be
+        .true;
+      expect(arraysEqual([0, 0, 0], res.activeGame.shipPositions[2])).to.be
+        .true;
+      expect(arraysEqual([0, 0, 0], res.activeGame.shipPositions[3])).to.be
+        .true;
+      expect(arraysEqual([0, 0, 0], res.activeGame.shipPositions[4])).to.be
+        .true;
+      expect(arraysEqual(shot1, res.activeGame.shots[0])).to.be.true;
+      expect(arraysEqual(shot2, res.activeGame.shots[1])).to.be.true;
+    }).timeout(TIMEOUT);
   });
 });
